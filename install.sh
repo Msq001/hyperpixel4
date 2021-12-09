26839823da2b355e17f7aad3bbd5a69ba6c64bca
Msq001 <760675063@qq.com>
2021-12-09 09:45:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ CONFIG_LINES=(
 	"display_rotate=0"
 	"dpi_group=2"
 	"dpi_mode=87"
-	"dpi_output_format=0x7f216"
+	"dpi_output_format=0x7f016"
 	"hdmi_timings=800 0 10 16 59 480 0 15 113 15 0 0 0 60 0 32000000 6"
 )
 
